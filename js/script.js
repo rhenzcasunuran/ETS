30d65bedc3f73b981488348365c451101b57fa3d
rhenzcasunuran <72377248+rhenzcasunuran@users.noreply.github.com>
2023-05-17 22:57:41 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ const closeBtn = document.querySelector("#btn");
 window.onload = function(){
 
     closeBtn.addEventListener("click",function(){
-        sidebar.classList.toggle("open")
+        if(sidebar.classList.toggle("open")){
+            openNav()
+        }
+        else{
+            closeNav()
+        }
+        
         menuBtnChange()
     })
 
@@ -13,6 +19,22 @@ window.onload = function(){
             closeBtn.classList.replace("bx-arrow-to-right","bx-arrow-to-left")
         }else{
             closeBtn.classList.replace("bx-arrow-to-left","bx-arrow-to-right")
+        }
+    }
+
+    function openNav() {
+        // If localStorage is supported by the browser
+        if (typeof(Storage) !== "undefined") {
+            // Save the state of the sidebar as "open"
+            localStorage.setItem("sidebar", "open");
+        }
+    }
+    
+    function closeNav() {
+        // If localStorage is supported by the browser
+        if (typeof(Storage) !== "undefined") {
+            // Save the state of the sidebar as "open"
+            localStorage.setItem("sidebar", "closed");
         }
     }
 }
