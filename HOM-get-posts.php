464a4c06cdfaf69466894afa1df8c52483601a79
rhenzcasunuran <72377248+rhenzcasunuran@users.noreply.github.com>
2023-05-19 00:38:48 +0800

    Delete HOM-get-posts.php

--- HOM-get-posts.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $dbname=mysqli_connect("localhost","root","","pupets");
-
-    $post_query = "SELECT * FROM post ORDER BY post_id DESC";
-    $get_posts = mysqli_query($dbname, $post_query);
-?>
