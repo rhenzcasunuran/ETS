483ce6eb3522ab88a9b435dca3b5c472ce1d074c
lancedalanon <133821754+Contradictive88@users.noreply.github.com>
2023-05-19 21:42:09 +0800

    Add files via upload

--- php/CAL-get-events-calendar.php
+++ php/CAL-get-events-calendar.php
@@ -14,32 +14,26 @@ $year = $_GET['year'];
 $month = $_GET['month'];
 $filters = $_GET['filters'];
 
-// If no filters are selected, use a simpler query
-if (empty($filters)) {
-    $sql = "SELECT * FROM listofeventtb WHERE YEAR(event_date) = ? AND MONTH(event_date) = ?
-    UNION
-    SELECT * FROM eventhistorytb WHERE YEAR(event_date) = ? AND MONTH(event_date) = ?";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, 'ss', $year, $month);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-} else {
-    // Build the parameter placeholders for the filters
-    $placeholders = implode(',', array_fill(0, count($filters), '?'));
-    $sql = "SELECT * FROM (
-                SELECT * FROM listofeventtb 
-                UNION ALL
-                SELECT * FROM eventhistorytb
-            ) AS combined_tables
-            WHERE YEAR(event_date) = ? AND MONTH(event_date) = ? AND event_type IN ($placeholders)";
-    $stmt = mysqli_prepare($conn, $sql);
-    $params = array_merge([$year, $month], $filters);
-    $types = str_repeat('s', count($params));
-    mysqli_stmt_bind_param($stmt, $types, ...$params);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-}
+// Build the parameter placeholders for the filters
+$placeholders = implode(',', array_fill(0, count($filters), '?'));
+$sql = "SELECT combined_table.event_id, combined_table.event_name, combined_table.event_type,
+        combined_table.category_name, combined_table.event_description, combined_table.event_date,
+        TIME_FORMAT(combined_table.event_time, '%h:%i %p') 
+        AS event_time
+        FROM (
+            SELECT event_id, event_name, event_type, category_name, event_description, event_date, event_time
+                FROM listofeventtb
+                    UNION ALL
+                    SELECT event_history_id, event_name, event_type, category_name, event_description, event_date, event_time
+                FROM eventhistorytb
+        ) AS combined_table
+        WHERE YEAR(combined_table.event_date) = ? AND MONTH(combined_table.event_date) = ? AND combined_table.event_type IN ($placeholders);";
+$stmt = mysqli_prepare($conn, $sql);
+$params = array_merge([$year, $month], $filters);
+$types = str_repeat('s', count($params));
+mysqli_stmt_bind_param($stmt, $types, ...$params);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 
 // Check if there are any results
 if (mysqli_num_rows($result) > 0) {
