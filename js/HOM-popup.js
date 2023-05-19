68420d582b8b51b75e1cebf1d4a4f7ebf19b856c
rhenzcasunuran <72377248+rhenzcasunuran@users.noreply.github.com>
2023-05-20 00:35:14 +0800

    Add files via upload

--- js/HOM-popup.js
+++ js/HOM-popup.js
@@ -14,10 +14,26 @@ var hide_clear = function(){
     clearPopup.style.display = 'none';
 }
 
+postPopup = document.getElementById('post-popup');
+var show_post = function(){
+    postPopup.style.display = 'flex';
+}
+var hide_post = function(){
+    postPopup.style.display = 'none';
+}
+
 discardChangesPopup = document.getElementById('discardChanges-popup');
 var show_discardChanges = function(){
     discardChangesPopup.style.display = 'flex';
 }
 var hide_discardChanges = function(){
     discardChangesPopup.style.display = 'none';
+}
+
+saveChangesPopup = document.getElementById('saveChanges-popup');
+var show_saveChanges = function(){
+    saveChangesPopup.style.display = 'flex';
+}
+var hide_saveChanges = function(){
+    saveChangesPopup.style.display = 'none';
 }
