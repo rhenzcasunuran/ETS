ca118ea4f49cb7378e72d4c94bd9d54ec0397348
Cvincent15 <116704777+Cvincent15@users.noreply.github.com>
2023-05-19 12:07:04 +0800

    Add files via upload

--- P&J-admin-formPJ.php
+++ P&J-admin-formPJ.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&amp;display=swap">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css'/>
     <link rel="stylesheet" href="css/P&J-participantsandjudges.css?v=<?php echo time(); ?>"> 
+    <link rel="stylesheet" href="./css/HOM-style.css">
     
     
 
@@ -222,6 +223,57 @@
     </div>
     
     <!--Page Content-->
+              <div class="container-fluid" id="popup">
+                <div class="row popup-card">
+                  <i class='row warning-icon bx bx-trash' ></i>
+                  <h3 class="row d-flex justify-content-center align-items-center">
+                    Delete Tables?
+                  </h3>
+                  <h6 class="row d-flex justify-content-center align-items-center">
+                    You cannot undo this action.
+                  </h6>
+                  <div class="row">
+                    <div class="col">
+                      <a href="HOM-manage-post.php?eed=<?php echo $count[0]?>" class="text-decoration-none" onclick="hide()">
+                        <div id="clear" class="button-clone continue" onclick="hide()">
+                          &nbsp;Continue
+                        </div>
+                      </a>
+                    </div>
+                    <div class="col">
+                      <div class="button-clone cancel" onclick="hide()">
+                        &nbsp;Cancel
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="container-fluid" id="popuplink">
+                <div class="row popup-card" style="width:450px; height:200px;">
+                <h6>Your Score Tabulation Link is Ready! <button onclick="hidel()" class="buttonsubx" style="position:relative; top:0; left:50px;">X</button></h6> 
+                  
+                <div class="col">
+                      
+                    </div>
+                    <div class="scorelink-container" >
+                    <p>Copy the link to score.</p>
+                    <textarea disabled>Https//:SampleLink</textarea>
+                <a href='P&J-admin-scoretab.php' target="_blank" style="position:absolute;top:0; right:0;">
+                <button class="buttonadd"id="clearlink">
+                 <i class='bx bxs-copy' ></i>
+                </button>
+                </a>
+                <h6>Only authorized persons can access the link.</h6>
+                </div>
+                  <div class="row">
+                    <div class="col">
+                      <a href="P&J-admin-formPJ.php?eed=<?php echo $count[0]?>" class="text-decoration-none" onclick="hidel()">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
     <section class="home-section">
       <div class="container">
         <div class="row" style="min-width: 100%;">
@@ -249,7 +301,7 @@
                           <div class="col"><label class="col-form-label" style="font-weight:1000;margin-top: 25px;margin-left: 11px; color:var(--color-content-text);">Judges</label></div>
                           <div class="col text-end" style="margin-right: 18px;">
                             <button onClick="add_judge();" class="buttonadd" type="button" id="judgeadd"><i class='bx bxs-user-plus'></i></button>
-                            <button onClick="deleteAll();" class="buttonsub" type="button" id="judgedel"><i class='bx bx-trash' ></i></button>
+                            <button  onClick="show()" class="buttonsub" type="button" id="judgedel"><i class='bx bx-trash' ></i></button>
                           </div>
                       </div>
                         <div style="border-radius: 20px;min-height: 300px; margin: auto; margin-top: 2px;width: auto;height: 100px;display: table;">
@@ -269,7 +321,7 @@
                                           <br>
                                           <input type='text' class='inputjname' name='judge_name[]' placeholder='Judge Name' Required/>
                                           <input type='text' class='inputjnick' name='judge_nick[]' placeholder='Nickname'Required/>
-                                          <a href='P&J-admin-scoretab.php'><button class='buttonlink1' type='button' style='border-radius:15px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button></a>
+                                          <button onClick='showl()' class='buttonlink1' type='button' style='border-radius:15px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button>
                                           <button class='buttonlink1 delJ btndel' id='deleteJA' style='margin-left: 70px;'><i class='bx bxs-trash-alt' ></i></button>
                                           <br>
                                         </li>
@@ -277,7 +329,7 @@
                                         <br>
                                           <input type='text' class='inputjname' name='judge_name[]' placeholder='Judge Name' Required/>
                                           <input type='text' class='inputjnick' name='judge_nick[]' placeholder='Nickname'Required/>
-                                          <a href='P&J-admin-scoretab.php'><button class='buttonlink1;' type='button' style='border-radius:15px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button></a>
+                                          <button onClick='showl()' class='buttonlink1;' type='button' style='border-radius:15px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button>
                                           <button class='buttonlink1 delJ btndel' id='deleteJB' style='margin-left: 70px;'><i class='bx bxs-trash-alt' ></i></button>
                                           <br/>
                                         </li>
@@ -302,7 +354,7 @@
                                               <label class="col text-end" style="margin-top: 25px;margin-left: 11px; color:var(--color-content-text); font-weight:1000;">Participants</label>
                                             <div class="col text-end" style="margin-right: 18px;">
                                               <button onClick="add_par();" class="buttonadd" id="paradd" type="button" ><i class='bx bxs-user-plus'></i></button>
-                                              <button class="buttonsub delP" id="pardel" type="button"><i class='bx bx-trash' ></i></button>
+                                              <button onClick="show()" class="buttonsub delP" id="pardel" type="button"><i class='bx bx-trash' ></i></button>
                                             </div>
                                             
                                             </div>
@@ -310,7 +362,7 @@
                                             <label class="col text-end" style="margin-top: 25px;margin-left: 11px; color:var(--color-content-text); font-weight:1000;">Participants</label>
                                             <div class="col text-end" style="margin-right: 18px;">
                                             <button onClick="add_par();" class="buttonaddg" type="button"><i class='bx bxs-group'></i></button>
-                                              <button class="buttonsub delP" id="pardel" type="button"><i class='bx bx-trash' ></i></button>
+                                              <button onClick="show()" class="buttonsub delP" id="pardel" type="button"><i class='bx bx-trash' ></i></button>
                                             </div>
                                             
                                             </div>
@@ -327,9 +379,9 @@
                                   <div class="col">
                                     <table>
                                   <th><h6 style="color: white; margin-left:30px; font-weight:1000; color:var(--color-content-text);">Participant Name</h6></th>
-                                  <th><h6 style="color: white; margin-left:50px; font-weight:1000; color:var(--color-content-text);">Course</h6></th>
-                                  <th><h6 style="color: white; margin-left:33px; font-weight:1000; color:var(--color-content-text);">Section</h6></th>
-                                  <th><h6 style="color: white; margin-left:65px; font-weight:1000; color:var(--color-content-text);">Organization</h6></th>
+                                  <th><h6 style="color: white; margin-left:60px; font-weight:1000; color:var(--color-content-text);">Course</h6></th>
+                                  <th><h6 style="color: white; margin-left:50px; font-weight:1000; color:var(--color-content-text);">Section</h6></th>
+                                  <th><h6 style="color: white; margin-left:70px; font-weight:1000; color:var(--color-content-text);">Organization</h6></th>
                                   </table>
                                   </div>
                                     <ul id="Pbox">
@@ -361,14 +413,12 @@
                                   <div class="col text-end" style="margin-top:30px;"><button class="buttonsave" type="button">Save</button><button class="buttoncancel" type="button">Cancel</button></div>
                                   </div>
                                   <div class="col group hide">
-                                  <div class="dropdown"><select class="btn dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="border-radius: 20px;width: 180.031px;margin-left: 30px;background: var(--bs-light);color: var(--bs-body-color);">Dropdown
-                                    <option>ITDS</option>
-                                    <option>GROUP B</option>
-                                    </select>
-                        
-                                    <select name ="Organization" class='btn dropdown-toggle' id="DropDownListID" aria-expanded='false' data-bs-toggle='dropdown' type='button' style='border-radius: 20px;width: 180.031px;margin-left: 50px;background: var(--bs-light);color: var(--bs-body-color);'>
-                                    <option>ELITE</option>
-                                    <option>JPIA</option>
+                                  <div class="">
+                                  <input type='text' class='inputpname' placeholder='ITDS' style="margin-left:30px;width: 180px; height: 40px;" Required/>
+                                    
+                                  <select form="orgInput" name ="Organization" class='btn dropdown-toggle orgOption' aria-expanded='false' data-bs-toggle='dropdown' type='button' style='border-radius: 20px;width: 180.031px;margin-left: 50px;background: var(--bs-light);color: var(--bs-body-color);'>
+                                    <option value="ELITE" class="ELITE">ELITE</option>
+                                    <option value="JPIA" class="JPIA">JPIA</option>
                                     </select>
                                     <button onClick="#" class="buttonadd" id="paradd" type="button" style="margin-left:180px;"><i class='bx bxs-user-plus'></i></button>
                                   </div>
@@ -383,7 +433,7 @@
                                       <input type='text' class='inputpname' placeholder='Participants Name' Required/>
                                           <input type='text' class='inputpcs' placeholder='Course' Required/>
                                           <input type='text' class='inputpcs' placeholder='Section' Required/>
-                                          <label id="LabelID" style="margin-left:40px; margin-right: 70px;color:white;font-weight:1000; color:var(--color-content-text);">ELITE</label>
+                                          <label class="orgChanged" style="margin-left:40px; margin-right: 70px;color:white;font-weight:1000; color:var(--color-content-text);">ELITE</label>
                                           <button class='buttonlink1 delP btndel' id='deleteP'><i class='bx bxs-trash-alt' ></i></button></i><br/>
                                       </li>
                                       <li style='list-style-type: none;'>
@@ -391,16 +441,14 @@
                                           <input type='text' class='inputpname' placeholder='Participants Name' Required/>
                                           <input type='text' class='inputpcs' placeholder='Course' Required/>
                                           <input type='text' class='inputpcs' placeholder='Section' Required/>
-                                          <label id="LabelID" style="margin-left:40px; margin-right: 70px;color:white; font-weight:1000; color:var(--color-content-text);">ELITE</label>
+                                          <label class="orgChanged" style="margin-left:40px; margin-right: 70px;color:white; font-weight:1000; color:var(--color-content-text);">ELITE</label>
                                           <button class='buttonlink1 delP btndel' id='deleteP'><i class='bx bxs-trash-alt' ></i></button></i><br/>
                                       </li>
                                     </ul>
                                   </div>
                                   <br>
-                                  <div class="dropdown"><select class="btn dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="border-radius: 20px;width: 180.031px;margin-left: 30px;background: var(--bs-light);color: var(--bs-body-color);">Dropdown
-                                    <option>GROUP B</option>
-                                    <option>ITDS</option>
-                                    </select>
+                                  <div class="dropdown">
+                                    <input type='text' class='inputpname' placeholder='ITDS' style="margin-left:30px;width: 180px; height: 40px;" Required/>
                         
                                     <select name ="Organization" class='btn dropdown-toggle' aria-expanded='false' data-bs-toggle='dropdown' type='button' style='border-radius: 20px;width: 180.031px;margin-left: 50px;background: var(--bs-light);color: var(--bs-body-color);'>
                                     <option>JPIA</option>
@@ -432,6 +480,7 @@
     <script src="./js/change-theme.js"></script>
     <script src="./js/jquery-3.6.4.js"></script>
     <script src="./js/jqueryPJ.min.js"></script>
+    <script src="./js/HOM-popup.js"></script>
     <script type="text/javascript">
       $('.menu_btn').click(function (e) {
         e.preventDefault();
@@ -451,13 +500,63 @@
     </script>
 
 <script>
+  popup = document.getElementById('popup');
+var show = function(){
+    popup.style.display = 'flex';
+}
+var hide = function(){
+    popup.style.display = 'none';
+}
+
+clearPopup = document.getElementById('clear-popup');
+var show_clear = function(){
+    clearPopup.style.display = 'flex';
+}
+var hide_clear = function(){
+    clearPopup.style.display = 'none';
+}
+
+discardChangesPopup = document.getElementById('discardChanges-popup');
+var show_discardChanges = function(){
+    discardChangesPopup.style.display = 'flex';
+}
+var hide_discardChanges = function(){
+    discardChangesPopup.style.display = 'none';
+}
+
+popupl = document.getElementById('popuplink');
+var showl = function(){
+    popupl.style.display = 'flex';
+}
+var hidel = function(){
+    popupl.style.display = 'none';
+}
+
+clearPopupl = document.getElementById('clear-popup');
+var show_clearl = function(){
+    clearPopupl.style.display = 'flex';
+}
+var hide_clearl = function(){
+    clearPopupl.style.display = 'none';
+}
+
+discardChangesPopupl = document.getElementById('discardChanges-popup');
+var show_discardChangesl = function(){
+    discardChangesPopupl.style.display = 'flex';
+}
+var hide_discardChangesl = function(){
+    discardChangesPopupl.style.display = 'none';
+}
+</script>
+
+<script>
 var intParTextBox = 0;
 var intJudgeTextBox = 0;
   function add_judge(){
     intJudgeTextBox++;
     var objNewJDiv = document.createElement('ul');
     objNewJDiv.setAttribute('id', 'ul_' + intJudgeTextBox);
-    objNewJDiv.innerHTML = "<div class='row append_judges'><li style='list-style-type: none;'><br><input type='text' class='inputjname' name='judge_name[]' placeholder='Judge Name' /><input type='text' class='inputjnickd' name='judge_nick[]' placeholder='Nickname'/><a href='P&J-admin-scoretab.php'><button class='buttonlink1;' type='button' style='border-radius:15px;margin-left: 3px;margin-right:5px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button></a><button class='buttonlink1 delJ btndel' id='deleteJ' style='margin-left: 70px;'><i class='bx bxs-trash-alt' ></i></button><br/></li></div>";
+    objNewJDiv.innerHTML = "<div class='row append_judges'><li style='list-style-type: none;'><br><input type='text' class='inputjname' name='judge_name[]' placeholder='Judge Name' /><input type='text' class='inputjnickd' name='judge_nick[]' placeholder='Nickname'/><button onClick='showl()'class='buttonlink1;' type='button' style='border-radius:15px;margin-left: 3px;margin-right:5px;width: 160px; height: 40px; color: white;background: #73A9CC;'><i class='bx bx-link'></i>Generate Link</button><button class='buttonlink1 delJ btndel' id='deleteJ' style='margin-left: 70px;'><i class='bx bxs-trash-alt' ></i></button><br/></li></div>";
     document.getElementById('Jbox').appendChild(objNewJDiv);  
   }
 
@@ -467,16 +566,16 @@ var intJudgeTextBox = 0;
     intJudgeTextBox--;
 });
 
-$(document).ready(
-  function() {
-    $('#DropdownListID').change(
-      function(){
+$("ul").on("click", "#judgedel", function(e) {
+    e.preventDefault();
+    $(".Jbox").parent().remove();
+    intJudgeTextBox--;
+});
 
-        $('#LabelID').text($('option:selected',this).text());
-      }
-      );
-  }
-  );
+$('.orgOption').on('change', function(){
+    var v = $(this).find(':selected').text();
+    $('.orgChanged').text( v );
+});
 
 function deleteAll(){
            var todo =  document.getElementByID("Jbox");
