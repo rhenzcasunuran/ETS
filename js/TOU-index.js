51a2a15004e2fd7a8a4cd46218afd861706ad9a9
Zeph04 <69961032+Zeph04@users.noreply.github.com>
2023-05-25 19:05:46 +0800

    Add files via upload

--- js/TOU-index.js
+++ js/TOU-index.js
@@ -1,50 +1,61 @@
-// Home status 
-
+// Home status
 let homeCount = 0;
 let changeCount = document.getElementById("home--btn");
+let decreaseOneButton = document.getElementById("btn--one")
 
 function plusOne() {
     homeCount += 1;
     changeCount.textContent = homeCount;
+    decreaseOneButton.disabled = false;
 }
+
 function plusTwo() {
     homeCount += 2;
     changeCount.textContent = homeCount;
+    decreaseOneButton.disabled = false;
 }
+
 function plusThree() {
     homeCount += 3;
     changeCount.textContent = homeCount;
+    decreaseOneButton.disabled = false;
 }
 
 function minusValueOne() {
-    var valueElement = document.getElementById("home--btn");
-    var homeCount = parseInt(valueElement.textContent);
-
     if (homeCount > 0) {
-        valueElement.textContent = homeCount - 1;
+        homeCount -= 1;
+        changeCount.textContent = homeCount;
     }
+
+    if (homeCount === 0) {
+        decreaseOneButton.disabled = true; // Disable the button
+    }
+
 }
 
 function minusValueTwo() {
-    var valueElement = document.getElementById("home--btn");
-    var guestCount = parseInt(valueElement.textContent);
+    if (homeCount > 1) {
+        homeCount -= 2;
+        changeCount.textContent = homeCount;
+    }
 
-    if (homeCount > 0) {
-        valueElement.textContent = homeCount - 2;
+    if (homeCount === 0) {
+        decreaseOneButton.disabled = true; // Disable the button
     }
 }
 
 function minusValueThree() {
-    var valueElement = document.getElementById("home--btn");
-    var homeCount = parseInt(valueElement.textContent);
+    if (homeCount > 2) {
+        homeCount -= 3;
+        changeCount.textContent = homeCount;
+    }
 
-    if (homeCount > 0) {
-        valueElement.textContent = homeCount - 3;
+    if (homeCount === 0) {
+        decreaseOneButton.disabled = true; // Disable the button
     }
 }
 
-
-// Guest status 
+// Guest status
 let guestCount = 0;
 let guestCountChange = document.getElementById("guest--btn");
 
@@ -52,66 +63,69 @@ function guestPlusOne() {
     guestCount += 1;
     guestCountChange.textContent = guestCount;
 }
+
 function guestPlusTwo() {
     guestCount += 2;
     guestCountChange.textContent = guestCount;
 }
+
 function guestPlusThree() {
     guestCount += 3;
     guestCountChange.textContent = guestCount;
 }
 
-
 function decreaseValueOne() {
-    var valueElement = document.getElementById("guest--btn");
-    var guestCount = parseInt(valueElement.textContent);
-
     if (guestCount > 0) {
-        valueElement.textContent = guestCount - 1;
+        guestCount -= 1;
+        guestCountChange.textContent = guestCount;
     }
 }
 
 function decreaseValueTwo() {
-    var valueElement = document.getElementById("guest--btn");
-    var guestCount = parseInt(valueElement.textContent);
-
-    if (guestCount > 0) {
-        valueElement.textContent = guestCount - 2;
+    if (guestCount > 1) {
+        guestCount -= 2;
+        guestCountChange.textContent = guestCount;
     }
 }
 
 function decreaseValueThree() {
-    var valueElement = document.getElementById("guest--btn");
-    var guestCount = parseInt(valueElement.textContent);
-
-    if (guestCount > 0) {
-        valueElement.textContent = guestCount - 3;
+    if (guestCount > 2) {
+        guestCount -= 3;
+        guestCountChange.textContent = guestCount;
     }
 }
 
-
-
-
-
-
-// save items 
-
+// Save items
 let saveCountHome;
 let saveCountGuest;
-let domHome = document.getElementById("home--count")
-let domGuest = document.getElementById("guest--count")
+let domHome = document.getElementById("home--count");
+let domGuest = document.getElementById("guest--count");
 
 function saveClick() {
-    saveCountHome  = homeCount + " -- "
+    saveCountHome = homeCount + " -- ";
     domHome.textContent += saveCountHome;
 
-    saveCountGuest  = guestCount + " -- "
+    saveCountGuest = guestCount + " -- ";
     domGuest.textContent += saveCountGuest;
 
-    changeCount.textContent = 0;
-    homeCount = 0;
-    guestCountChange.textContent = 0;
-    guestCount = 0;
 
-}
+   // Save the current text displayed in localStorage
+  // localStorage.setItem("savedHomeText", domHome.textContent);
+  // localStorage.setItem("savedGuestText", domGuest.textContent); 
+} 
 
+
+
+// Retrieve the saved text when the page loads
+/* window.addEventListener("load", function () {
+    const savedHomeText = localStorage.getItem("savedHomeText");
+    const savedGuestText = localStorage.getItem("savedGuestText");
+
+    if (savedHomeText) {
+        domHome.textContent = savedHomeText;
+    }
+
+    if (savedGuestText) {
+        domGuest.textContent = savedGuestText;
+    }
+}); */
