23753c7cf9201ccc3238365684fd062d26bc6783
Cvincent15 <116704777+Cvincent15@users.noreply.github.com>
2023-06-13 01:41:18 +0800

    Add files via upload

--- P&J-admin-formPJ.php
+++ P&J-admin-formPJ.php
@@ -52,199 +52,11 @@ th, td {
 
   </head>
   <body>
-  <div class="sidebar open box-shadow">
-      <div class="bottom-design">
-        <div class="design1"></div>
-        <div class="design2"></div>
-      </div>
-      <div class="logo_details">
-        <img src="./pictures/logo.png" alt="student council logo" class="icon logo">
-        <div class="logo_name">Events Tabulation System</div>
-        <i class="bx bx-arrow-to-right" id="btn"></i>
-        <script src="./js/sidebar-state.js"></script>
-      </div>
-      <div class="wrapper">
-        <li class="nav-item top">
-          <a href="index.php">
-            <i class="bx bx-home-alt"></i>
-            <span class="link_name">Go Back</span>
-          </a>
-        </li>
-        <div class="sidebar-content-container">
-          <ul class="nav-list">
-            <li class="nav-item">
-              <a href="#posts" class="menu_btn">
-                <i class="bx bx-news"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Posts
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="HOM-create-post.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Create Post</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="HOM-draft-scheduled-post.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Draft & Scheduled Post</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="HOM-manage-post.php">
-                    <i class="bx bxs-circle sub-icon color-yellow"></i>
-                    <span class="sub_link_name">Manage Post</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#event_menu" class="menu_btn">
-                <i class="bx bx-calendar-edit"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Events
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="EVE-admin-list-of-events.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">List of Events</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="EVE-admin-event-configuration.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Event Configuration</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="#criteria_config">
-                    <i class="bx bxs-circle sub-icon color-yellow"></i>
-                    <span class="sub_link_name">Criteria Configuration</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="menu_btn">
-                <i class="bx bx-calendar"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Calendar
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="CAL-admin-overall.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Overview</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="CAL-admin-logs.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Logs</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="BAR-admin.php">
-                <i class='bx bx-bar-chart-alt-2'></i>
-                <span class="link_name">Overall Results</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#tournaments" class="menu_btn">
-                <i class="bx bx-trophy"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Tournaments
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="TOU-Live-Scoring-Admin.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Live Scoring</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="TOU-bracket-admin.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Manage Brackets</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#competition" class="menu_btn">
-                <i class="bx bx-medal"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Competition
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="COM-manage_results_page.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Manage Results</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="COM-tobepublished_page.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">To Publish</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="COM-published_page.php">
-                    <i class="bx bxs-circle sub-icon color-yellow"></i>
-                    <span class="sub_link_name">Published Results</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="#archive">
-                    <i class="bx bxs-circle sub-icon color-purple"></i>
-                    <span class="sub_link_name">Archive</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#event_history" class="menu_btn">
-                <i class="bx bx-history"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Event History
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="HIS-admin-ManageEvent.php">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Event Page</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="HIS-admin-highlights.php">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Highlights Page</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="P&J-admin-formPJ.php" class="menu_btn active">
-                <i class="bx bx-group"></i>
-                <span class="link_name">Judges & <br> Participants</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    
+    <?php
+      $activeModule = 'judges-participants';
+
+      require 'php/admin-sidebar.php';
+    ?>
     <!--Page Content-->
     <!--Popup Confirm / Success for submit all button-->
     <div class="popup-background" id="submitWrapper">
@@ -364,13 +176,41 @@ th, td {
                                     
                                   </div>
                                   <div>
-                                  <table>
+                                  <table class="table" style="text-align:center; margin-left:40px; margin-top:1px;">
+                                  <thread>
                                     <tr>
-                                  <th><h6 class="judgeheader">Judge Name</h6></th>
-                                  <th><h6 class="judgeheader">Judge Nickname</h6></th>
-                                  <th><h6 class="judgeheader">Scoring Link</h6></th>
+                                  <th><h6 class='judgeheader'>Judge Name</h6></th>
+                                  <th><h6 class='judgeheader'>Judge Nickname</h6></th>
+                                  <th><h6 class='judgeheader'>Scoring Link</h6></th>
                                   </tr>
-                                  </table>
+                                  </thread>
+<!-- Judge Form -->                 
+                                     <tbody>
+                                      <?php
+                                      $sql = "SELECT * FROM pjjudgestemp";
+                                      $result = $conn -> query($sql);
+
+                                      if (!$conn) {
+                                        die("Connection Failed: " . mysqli_connect_error());
+                                    }
+
+                                    while($row = $result -> fetch_assoc()) {
+
+                                      echo"
+                                      <tr>
+                                      <td>" . $row["judge_name_temp"] . "</td>
+                                      <td>" . $row["judge_nick_temp"] . "</td>
+                                      <td>https://sample.link</td>
+                                      <td>
+                                       <a class='btn btn-primary btn-sm' style='margin-left:30px;'>Edit</a>
+                                      </td>
+                                      </tr>";
+                                    }
+                                    
+                                      
+                                      ?>
+                                      </tbody>
+                                    </table>
                                     <ul id="Jbox">
 <!-- Judge Form -->
                                       <div class="append_judges">
@@ -449,12 +289,42 @@ th, td {
                                   <div class="col">
 <!-- Participants Form Individual -->
                                   </div>
-                                  <table>
+                                  <table class="table" style="text-align:center; margin-left:10px; margin-top:1px;">
+                                  <thread>
+                                    <tr>
                                   <th><h6 style=" margin-left:30px; font-weight:1000; color:var(--color-content-text);">Participant Name</h6></th>
                                   <th><h6 style="color: white; margin-left:60px; font-weight:1000; color:var(--color-content-text);">Course</h6></th>
                                   <th><h6 style="color: white; margin-left:50px; font-weight:1000; color:var(--color-content-text);">Section</h6></th>
                                   <th><h6 style="color: white; margin-left:70px; font-weight:1000; color:var(--color-content-text);">Organization</h6></th>
-                                  </table>
+                                  </tr>
+                                  </thread>
+                                  <tbody>
+                                      <?php
+                                      $sql = "SELECT * FROM pjparticipantstemp";
+                                      $result = $conn -> query($sql);
+
+                                      if (!$conn) {
+                                        die("Connection Failed: " . mysqli_connect_error());
+                                    }
+
+                                    while($row = $result -> fetch_assoc()) {
+
+                                      echo"
+                                      <tr>
+                                      <td>" . $row["participants_name_temp"] . "</td>
+                                      <td>" . $row["participants_course_temp"] . "</td>
+                                      <td>" . $row["participants_section_temp"] . "</td>
+                                      <td>" . $row["participants_organization_temp"] . "</td>
+                                      <td>
+                                       <a class='btn btn-primary btn-sm' style='margin-left:30px;'>Edit</a>
+                                      </td>
+                                      </tr>";
+                                    }
+                                    
+                                      
+                                      ?>
+                                      </tbody>
+                                    </table>
                                   <form action="#" method="POST" id="add_form3">
                                     <ul id="Pbox">
                                       <li style='list-style-type: none;'>
@@ -489,6 +359,59 @@ th, td {
                                   </div>
                                   <div class="col group hide" style="width:auto;">
                                   <div>
+ <table class="table" style="color:var(--color-content-text);">
+    <thead>
+      <tr>
+        <?php
+                                      $sql = "SELECT * FROM pjparticipantsgrouptemp";
+                                      $result = $conn -> query($sql);
+
+                                      if (!$conn) {
+                                        die("Connection Failed: " . mysqli_connect_error());
+                                    }
+
+                                    while($row = $result -> fetch_assoc()) {
+       echo" <th>Group Name</th>
+       <th>" . $row["participants_organization_group_temp"] . " <a class='btn btn-primary btn-sm' style='margin-left:30px;'>Edit</a></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>" . $row["participants_name_group_temp"] . "</td>
+        <td>
+          <table>
+            <thead>
+              <tr>
+                <th>Member Name</th>
+                <th>Course</th>
+                <th>Section</th>
+              </tr>
+            </thead>";
+            $sql = "SELECT * FROM pjparticipantsgroupmemberstemp";
+                                      $result = $conn -> query($sql);
+
+                                      if (!$conn) {
+                                        die("Connection Failed: " . mysqli_connect_error());
+                                    }
+
+                                    while($row = $result -> fetch_assoc()) {
+            echo"<tbody>
+              <tr>
+                <td>" . $row["participants_name_g_temp"] . "</td>
+                <td>" . $row["participants_course_group_temp"] . "</td>
+                <td>" . $row["participants_section_group_temp"] . "</td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>";
+    }
+  }
+                                    
+                                      
+    ?>
+    </tbody>
+  </table>
                                   <form action="#" method="POST" id="add_form4">
                                     <ul id="Pboxg">
                                       <div class="Pargdel">
