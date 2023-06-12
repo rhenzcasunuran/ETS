fec364767f8aa5cbeb8dcfaed0c3d645384dd8c6
COM-DEV-Robbie <133989615+COM-DEV-Robbie@users.noreply.github.com>
2023-06-13 05:31:32 +0800

    Add files via upload
    nagdagdag ako ng isang column sa competitions_table ko

--- pupets.sql
+++ pupets.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2023 at 07:18 PM
+-- Generation Time: Jun 12, 2023 at 11:29 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `pupets`
@@ -121,19 +115,20 @@ CREATE TABLE `competitions_table` (
   `competition_id` int(11) NOT NULL,
   `competition_name` varchar(255) NOT NULL,
   `event_id` int(11) NOT NULL,
-  `schedule` datetime DEFAULT NULL
+  `schedule` datetime DEFAULT NULL,
+  `schedule_end` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `competitions_table`
 --
 
-INSERT INTO `competitions_table` (`competition_id`, `competition_name`, `event_id`, `schedule`) VALUES
-(1, 'Essay Writing', 1, '2023-05-11 11:09:00'),
-(2, 'Spoken Poetry', 1, '2023-12-22 04:17:00'),
-(3, 'Zumba', 2, NULL),
-(4, 'Cheerleading', 2, '2024-06-11 02:57:00'),
-(5, 'Eating Contest', 2, '2025-04-10 02:53:00');
+INSERT INTO `competitions_table` (`competition_id`, `competition_name`, `event_id`, `schedule`, `schedule_end`) VALUES
+(1, 'Essay Writing', 1, '2023-06-13 05:23:00', '2023-06-15 05:23:00'),
+(2, 'Spoken Poetry', 1, '2023-06-13 05:23:00', '2023-06-20 05:23:00'),
+(3, 'Zumba', 2, NULL, NULL),
+(4, 'Cheerleading', 2, NULL, NULL),
+(5, 'Eating Contest', 2, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -1260,7 +1255,3 @@ ALTER TABLE `teams`
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
