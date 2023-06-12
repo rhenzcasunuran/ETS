05f986f39c24988f56b6a7da67d45b5ad10b8d93
raphbarba <133987979+BenDoughBer@users.noreply.github.com>
2023-06-13 02:15:31 +0800

    Add files via upload

--- BAR-student.php
+++ BAR-student.php
@@ -21,6 +21,11 @@ if($conn){
     }
   }
 }
+
+    $activeModule = 'results';
+    $activeSubItem = 'overall-champion';
+
+    require './php/student-sidebar.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,149 +45,12 @@ if($conn){
     <link rel="stylesheet" href="./css/home-sidebar-style.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/BAR-obg.css">
+    <link rel="stylesheet" href="./css/system-wide.css">
     <script src="./js/BAR-java.js"></script>
   </head>
 
 
   <body>
-    <!--Sidebar-->
-    <div class="container-fluid" id="popup">
-      <div class="row popup-card">
-        <form method="post">
-          <div class="row">
-            <div class="col-11 admin-text">
-              <p>
-                Administrator
-              </p>
-            </div>
-            <div class="col-1 close ">
-              <i class='bx bx-x' onclick="hide()"></i>
-            </div>
-          </div>
-          <div class="row">
-            <input type="text" name="user_username" placeholder="Username" maxlength="20" required/>
-          </div>
-          <div class="row">
-            <input type="password" name="user_password" placeholder="Password" maxlength="128" required/>
-          </div>
-          <div class="row justify-content-center">
-            <button input type="submit" name="sign-in-button" class="sign-in-button">Sign In</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <!--SIDEBAR-->
-    <div class="sidebar open box-shadow">
-      <div class="bottom-design">
-        <div class="design1"></div>
-        <div class="design2"></div>
-      </div>
-      <div class="logo_details">
-        <img src="./pictures/logo.png" alt="student council logo" class="icon logo">
-        <div class="logo_name">Events Tabulation System</div>
-        <i class="bx bx-arrow-to-right" id="btn"></i>
-        <script src="./js/sidebar-state.js"></script>
-      </div>
-      <div class="wrapper">
-        <div class="sidebar-content-container">
-          <ul class="nav-list">
-            <li class="nav-item">
-              <a href="index.php">
-                <i class="bx bx-home-alt"></i>
-                <span class="link_name">Home</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="CAL-student-overall.php">
-                <i class="bx bx-calendar"></i>
-                <span class="link_name">Calendar</span>
-              </a>
-            </li>
-            <li class="nav-item">
-            <a href="#posts" class="menu_btn active">
-                <i class="bx bx-line-chart"><i class="dropdown_icon bx bx-chevron-down"></i></i>
-                <span class="link_name">Results
-                  <i class="change-icon dropdown_icon bx bx-chevron-right"></i>
-                </span>
-              </a>
-              <ul class="sub_list">
-                <li class="sub-item">
-                  <a href="BAR-student.php" class="sub-active">
-                    <i class="bx bxs-circle sub-icon color-red"></i>
-                    <span class="sub_link_name">Overall Champion</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="#tournament">
-                    <i class="bx bxs-circle sub-icon color-green"></i>
-                    <span class="sub_link_name">Tournament</span>
-                  </a>
-                </li>
-                <li class="sub-item">
-                  <a href="COM-student_page.php">
-                    <i class="bx bxs-circle sub-icon color-yellow"></i>
-                    <span class="sub_link_name">Competition</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            
-            <li class="nav-item">
-              <a href="HIS-student-index.php">
-                <i class="bx bx-history"></i>
-                <span class="link_name">Event History</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="about.php">
-                <i class="bx bx-info-circle"></i>
-                <span class="link_name">About</span>
-              </a>
-            </li>
-            <?php
-              if(isset($_SESSION['user_username'])){
-            ?>
-            <li class="nav-item">
-              <a href="HOM-create-post.php">
-                <i class="bx bx-cog"></i>
-                <span class="link_name">Configuration</span>
-              </a>
-            </li>
-            <?php
-              }
-            ?>
-          </ul>
-        </div>
-        <div class="bottom-container">
-          <div class="mode-btn" id="theme-toggle">
-            <i class='lightmode bx bx-sun'></i>
-            <i class='darkmode bx bx-moon'></i>
-          </div>
-          <?php
-            if(isset($_SESSION['user_username'])){
-          ?>
-            <li class="nav-item bottom">
-              <a href="./php/sign-out.php">
-                <i class="bx bx-log-out"></i>
-                <span class="link_name">Sign Out</span>
-              </a>
-            </li>
-          <?php
-            }
-            else{
-          ?>
-              <li class="nav-item bottom">
-                <a onclick="show()">
-                  <i class="bx bx-log-in"></i>
-                  <span class="link_name">Sign In</span>
-                </a>
-              </li>
-          <?php
-            }
-          ?>
-        </div>
-      </div>
-    </div>
     <!--Page Content-->
     <section class="home-section" style="display: flex; justify-content: center; align-items: center;">
       <div class="container-fluid" id="body-content">
