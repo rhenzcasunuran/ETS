705571bfdf2f3617c000eb0c19b89b21884c888d
rhenzcasunuran <72377248+rhenzcasunuran@users.noreply.github.com>
2023-06-23 07:00:29 +0800

    Add files via upload

---
+++ php/HOM-edit-draft.php
@@ -0,0 +1,20 @@
+<?php
+    include 'HOM-post-data.php';
+    include 'database_connect.php';
+
+    if(isset($_POST['post'])){
+        $calendar =  mysqli_real_escape_string($conn,$_POST['post_calendar']);
+        $tag =  mysqli_real_escape_string($conn,$_POST['post_tag']);
+        $title =  mysqli_real_escape_string($conn,$_POST['post_title']);
+        $description =  mysqli_real_escape_string($conn,$_POST['post_description']);
+        $current_date = date("y-m-d h:i:s");
+
+        $sql = "UPDATE post 
+                SET post_calendar = '$calendar', post_tag = '$tag', post_title = '$title', post_description = '$description', post_draft = '0'
+                WHERE post_id = $id";
+        mysqli_query($conn,$sql);
+        to_log($conn, $sql);
+
+        $_SESSION['message']="Successfully added to the database."; 
+    }
+?>
--- php/HOM-get-drafts.php
+++ php/HOM-get-drafts.php
@@ -1,6 +1,6 @@
 <?php
     include 'database_connect.php';
 
-    $post_query = "SELECT * FROM post WHERE post_draft = 0 ORDER BY post_schedule DESC";
+    $post_query = "SELECT * FROM post WHERE post_draft = 1 ORDER BY post_schedule DESC";
     $get_posts = mysqli_query($conn, $post_query);
 ?>
