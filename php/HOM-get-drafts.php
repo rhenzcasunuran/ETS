ef99c7cacbea21d7fe20beaa304adb9270d643f8
rhenzcasunuran <72377248+rhenzcasunuran@users.noreply.github.com>
2023-06-26 20:49:55 +0800

    Delete HOM-get-drafts.php

--- php/HOM-get-drafts.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include 'database_connect.php';
-
-    $post_query = "SELECT * FROM post WHERE post_draft = 1 ORDER BY post_schedule DESC";
-    $get_posts = mysqli_query($conn, $post_query);
-?>
