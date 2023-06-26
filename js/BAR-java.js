762881d7c8b0c0516f88e788afc6fa8ee7c94d10
raphbarba <133987979+BenDoughBer@users.noreply.github.com>
2023-06-27 00:43:06 +0800

    Add files via upload

--- js/BAR-java.js
+++ js/BAR-java.js
@@ -82,6 +82,7 @@ cancelWrapper.addEventListener("click", function(event) {
       const anon = document.getElementById("anon_button")
       const logos = document.querySelectorAll("#logos");
       const meters = document.querySelectorAll(".meter");
+      const logo_container = document.querySelectorAll("img")
 
       $.ajax({
         url: "./php/BAR-anon.php",
@@ -99,6 +100,10 @@ cancelWrapper.addEventListener("click", function(event) {
             meters.forEach(function(meter) {
               meter.setAttribute("id", "anon");
             });
+            logo_container.forEach(function(container) {
+              container.setAttribute("name", "anon");
+            });
+            
           } else {
             anon.checked = false;
             console.log("false")
@@ -112,6 +117,7 @@ cancelWrapper.addEventListener("click", function(event) {
                   const imagePath = "logos/" + organization + ".png";
                   const img = container.querySelector("img");
                   img.src = imagePath;
+                  img.setAttribute("name", organization)
                 });
                 meters.forEach(function (meter, index) {
                   const organization = organizations[index];
@@ -134,81 +140,61 @@ cancelWrapper.addEventListener("click", function(event) {
         const placementOpen = graphSection.classList.contains("placement_open");
         if (placementOpen) {
           let logoSrc;
-          let profileText;
-          let ulText;
-          let orgPhotoSrc;    
+          let profileText;  
 
           $.ajax({
             url: "./php/BAR-get-competition.php",
             type: "GET",
             dataType: "json",
-            success: function(response) {
-                console.log(response);
-                const competitionNames = response.map(item => item.competition_name);
+            success: function() {
 
                 switch (logoName) {
-                  case "acap":
+                  case "ACAP":
                     console.log("ACAP logo clicked");
                     logoSrc = "logos/ACAP.png";
                     profileText = "Association of Competent and Aspiring Psychologists";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/koala.jpg";
                     break;
-                  case "aeces":
+                  case "AECES":
                     console.log("AECES logo clicked");
                     logoSrc = "logos/AECES.png";
                     profileText = "Association of Electronics and Communications Engineering Students";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/fruits.jpg";
                     break;
-                  case "elite":
+                  case "ELITE":
                     console.log("ELITE logo clicked");
                     logoSrc = "logos/ELITE.png";
                     profileText = "Eligible League of Information Technology Enthusiasts";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/tower.jpg";
                     break;
-                  case "give":
+                  case "GIVE":
                     console.log("GIVE logo clicked");
                     logoSrc = "logos/GIVE.png";
                     profileText = "Guild of Imporous and Valuable Educators";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/give.jpg";
                     break;
-                  case "jehra":
+                  case "JEHRA":
                     console.log("JEHRA logo clicked");
                     logoSrc = "logos/JEHRA.png";
                     profileText = "Junior Executive of Human Resource Association";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/jehra.jpg";
                     break;
-                  case "jmap":
+                  case "JMAP":
                     console.log("JMAP logo clicked");
                     logoSrc = "logos/JMAP.png";
                     profileText = "Junior Marketing Association of the Philippines";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/jmap.jpg";
                     break;
-                  case "jpia":
+                  case "JPIA":
                     console.log("JPIA logo clicked");
                     logoSrc = "logos/JPIA.png";
                     profileText = "Junior Philippine Institute of Accountants";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/jpia.jpg";
                     break;
-                  case "piie":
+                  case "PIIE":
                     console.log("PIIE logo clicked");
                     logoSrc = "logos/PIIE.png";
                     profileText = "Philippine Institute of Industrial Engineers";
-                    ulText = 'Participated in: <li>' + competitionNames.join('</li><li>') + '</li>';
-                    orgPhotoSrc = "org-photos/tower.jpg";
                     break;
                   default:
-                    console.log(logoName + " logo clicked");
+                    console.log("lmao mali");
                     return;
                 }
             
-                const logoImage = document.querySelector("#profile-logo");
+                const logoImage = document.querySelector(".profile-logo");
                 if (logoImage) {
                   logoImage.src = logoSrc;
                 } else {
@@ -220,21 +206,6 @@ cancelWrapper.addEventListener("click", function(event) {
                   profileName.textContent = profileText;
                 } else {
                   console.log("Profile name element not found");
-                }
-            
-                const ulElement = document.querySelector(".winnings");
-                if (ulElement) {
-                  ulElement.innerHTML = ulText;
-                } else {
-                  console.log("UL element not found");
-                }
-            
-                const orgPhotoImage = document.querySelector(".org-photo-image");
-                if (orgPhotoImage) {
-                  orgPhotoImage.style.display = "block";
-                  orgPhotoImage.src = orgPhotoSrc;
-                } else {
-                  console.log("Org photo image element not found");
                 }
             },
             error: function(xhr) {
