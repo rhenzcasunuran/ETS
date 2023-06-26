0412bf9dd2c8e90be859837b21e3cdef27f06bca
raphbarba <133987979+BenDoughBer@users.noreply.github.com>
2023-06-26 21:47:29 +0800

    Add files via upload

--- php/BAR-organization.php
+++ php/BAR-organization.php
@@ -10,13 +10,13 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT organization FROM bar_graph ORDER BY barMeter DESC";
+$sql = "SELECT * FROM `bar_graph` INNER JOIN organization on bar_graph.organization_id = organization.organization_id ORDER BY bar_meter DESC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   $organizations = array();
   while ($row = $result->fetch_assoc()) {
-      $organizations[] = $row['organization'];
+      $organizations[] = $row['organization_name'];
   }
   // Return organization names as JSON
   echo json_encode($organizations);
