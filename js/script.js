316d561b0605db29c27c0d6da407d597ec93714f
Contradictive88 <133821754+Contradictive88@users.noreply.github.com>
2023-07-03 11:40:22 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,53 +1,36 @@
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector("#btn");
 
-window.onload = function(){
-
-    closeBtn.addEventListener("click",function(){
-        if(sidebar.classList.toggle("open")){
-            openNav()
-        }
-        else{
-            closeNav()
-        }
-        
-        menuBtnChange()
-    })
-
-    function menuBtnChange(){
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-arrow-to-right","bx-arrow-to-left")
-        }else{
-            closeBtn.classList.replace("bx-arrow-to-left","bx-arrow-to-right")
-        }
-    }
-
-    function openNav() {
-        // If localStorage is supported by the browser
-        if (typeof(Storage) !== "undefined") {
-            // Save the state of the sidebar as "open"
-            localStorage.setItem("sidebar", "open");
-        }
-    }
-    
-    function closeNav() {
-        // If localStorage is supported by the browser
-        if (typeof(Storage) !== "undefined") {
-            // Save the state of the sidebar as "open"
-            localStorage.setItem("sidebar", "closed");
-        }
-    }
+function openNav() {
+  // If localStorage is supported by the browser
+  if (typeof Storage !== "undefined") {
+    // Save the state of the sidebar as "open"
+    localStorage.setItem("sidebar", "open");
+  }
 }
 
-
-
-if (typeof(Storage) !== "undefined") {
-    // If we need to open the bar
-    if(localStorage.getItem("sidebar") == "open"){
-        // Open the bar
-        sidebar.classList.add("open");
-    }
-    else if (localStorage.getItem("sidebar") == "close"){
-        sidebar.classList.remove("open");
-    }
+function closeNav() {
+  // If localStorage is supported by the browser
+  if (typeof Storage !== "undefined") {
+    // Save the state of the sidebar as "closed"
+    localStorage.setItem("sidebar", "closed");
+  }
 }
+
+function menuBtnChange() {
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-arrow-to-right", "bx-arrow-to-left");
+  } else {
+    closeBtn.classList.replace("bx-arrow-to-left", "bx-arrow-to-right");
+  }
+}
+
+closeBtn.addEventListener("click", function () {
+  if (sidebar.classList.toggle("open")) {
+    openNav();
+  } else {
+    closeNav();
+  }
+
+  menuBtnChange();
+});
