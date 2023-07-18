f66680793737644b1edf0d77a8520bd750195147
Contradictive88 <lanceorville5@gmail.com>
2023-07-19 01:02:47 +0800

    Integrated to db

--- php/TOU-json-get-event-ids.php
+++ php/TOU-json-get-event-ids.php
@@ -11,28 +11,37 @@ INNER JOIN ongoing_event_name AS oen
   ON olfe.ongoing_event_name_id = oen.ongoing_event_name_id
 INNER JOIN tournament AS tou
   ON tou.event_id = olfe.event_id
-WHERE oen.event_name = 'Buwan ng Wika'
-AND olfe.category_name = 'Chess' 
+WHERE oen.event_name = ?
+AND olfe.category_name = ? 
 AND olfe.event_type_id = 1 
 AND oen.is_done = 0 
 AND olfe.is_archived = 0 
 AND tou.has_set_tournament = 0
-AND olfe.is_deleted = 0;";
+AND olfe.is_deleted = 0";
+
 $stmt = mysqli_prepare($conn, $query);
 mysqli_stmt_bind_param($stmt, "ss", $event_name, $category_name);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
-// Fetch the event IDs from the result
+// Fetch the event IDs and names from the result
 $eventIds = [];
+$eventNames = [];
 while ($row = mysqli_fetch_assoc($result)) {
     $eventIds[] = $row['event_id'];
+    $eventNames[] = $row['event_name'];
 }
 
 // Close the statement and database connection
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
 
-// Return the event IDs as a JSON response
-echo json_encode($eventIds);
+// Create an associative array with event IDs and names
+$response = [
+    'eventIds' => $eventIds,
+    'eventNames' => $eventNames
+];
+
+// Return the response as JSON
+echo json_encode($response);
 ?>
