ad7971c6a6117d8334a186e3dd3a8ee8fdba85b6
raphbarba <133987979+BenDoughBer@users.noreply.github.com>
2023-07-18 10:25:19 +0800

    Add files via upload

--- php/BAR-update-anon.php
+++ php/BAR-update-anon.php
@@ -7,10 +7,17 @@ header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
 include 'CAL-logger.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $isAnon = $_POST["isAnon"];
-  $query = "UPDATE bar_graph SET isAnon = ?";
+  $isAnon = isset($_POST["isAnon"]) ? $_POST["isAnon"] : 0;
+  $selectedEventName = isset($_POST['selectedEventName']) ? $_POST['selectedEventName'] : '';
+
+  $query = "UPDATE bar_graph 
+            INNER JOIN organization ON bar_graph.organization_id = organization.organization_id 
+            INNER JOIN ongoing_event_name ON bar_graph.ongoing_event_name_id = ongoing_event_name.ongoing_event_name_id
+            SET bar_graph.isAnon = ?
+            WHERE bar_graph.ongoing_event_name_id = ?";
+
   $stmt = $conn->prepare($query);
-  $stmt->bind_param("i", $isAnon);
+  $stmt->bind_param("is", $isAnon, $selectedEventName);
 
   if ($stmt->execute()) {
     echo json_encode(["success" => true]);
