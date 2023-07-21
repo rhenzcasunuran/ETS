2fca35033719cf4a4fbea1204b8d569c66341619
raphbarba <133987979+BenDoughBer@users.noreply.github.com>
2023-07-21 13:40:50 +0800

    Add files via upload

--- php/BAR-organization.php
+++ php/BAR-organization.php
@@ -4,27 +4,30 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
 
 @include 'database_connect.php';
+include 'CAL-logger.php';
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $selectedEventName = isset($_GET['selectedEventName']) ? $_GET['selectedEventName'] : '';
+
+    $sql = "SELECT * FROM `bar_graph`
+    INNER JOIN organization ON bar_graph.organization_id = organization.organization_id 
+    INNER JOIN ongoing_event_name ON bar_graph.ongoing_event_name_id = ongoing_event_name.ongoing_event_name_id
+    WHERE bar_graph.ongoing_event_name_id = $selectedEventName
+    ORDER BY bar_meter DESC;";
+
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      $organizations = array();
+      while ($row = $result->fetch_assoc()) {
+          $organizations[] = $row['organization_name'];
+      }
+      // Return organization names as JSON
+      echo json_encode($organizations);
+    } else {
+      echo "No organizations found in the database.";
+    }
+    mysqli_free_result($result);
 }
-
-$sql = "SELECT * FROM `bar_graph` INNER JOIN organization on bar_graph.organization_id = organization.organization_id ORDER BY bar_meter DESC";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  $organizations = array();
-  while ($row = $result->fetch_assoc()) {
-      $organizations[] = $row['organization_name'];
-  }
-  // Return organization names as JSON
-  echo json_encode($organizations);
-} else {
-  echo "No organizations found in the database.";
-}
-
-// Close the connection
 mysqli_close($conn);
 ?>
