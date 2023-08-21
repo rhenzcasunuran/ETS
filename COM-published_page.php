fa8e8c8689b5c51730773bcc68960d9e9dbfd9c8
robbie <133989615+COM-DEV-Robbie@users.noreply.github.com>
2023-08-22 07:12:22 +0800

    Nalimutan ko<?php include '.php/title.php' ?>hahah

--- COM-published_page.php
+++ COM-published_page.php
@@ -7,6 +7,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="page-id" content="published">
+      <?php include '.php/title.php' ?>
       <!-- Side Bar CSS -->
       <link rel="stylesheet" href="./css/boxicons.css">
       <link rel="stylesheet" href="./css/COM-theme-mode.css">
--- COM-student_page.php
+++ COM-student_page.php
@@ -29,6 +29,7 @@ if($conn){
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="page-id" content="comstudent">
+      <?php include '.php/title.php' ?>
       <!-- Side Bar CSS -->
       <link rel="stylesheet" href="./css/boxicons.css">
       <link rel="stylesheet" href="./css/COM-theme-mode.css">
