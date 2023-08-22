00b8e779e071794fd94a6491cce6e3ae5399845f
Lrac <90054175+Clbantatua@users.noreply.github.com>
2023-08-22 23:53:58 +0800

    Add files via upload

--- HIS-admin-ManageEvent.php
+++ HIS-admin-ManageEvent.php
@@ -6,7 +6,7 @@ include './php/database_connect.php';
 <html lang="en">
 
 <head>
-<?php include '.php/title.php' ?> 
+<?php include 'php/title.php' ?> 
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
